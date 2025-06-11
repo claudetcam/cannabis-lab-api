22cd0373da4fa5ad34d9fcb4c9ad2095975682bd
claudetcam <claudetcamille@gmail.com>
2025-06-11 15:07:15 -0300

    Update install_chrome.sh

--- install_chrome.sh
+++ install_chrome.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
-set -x
 
 # Installer les dépendances
-apt-get update && apt-get install -y wget curl unzip gnupg2
+apt-get update
+apt-get install -y wget gnupg unzip
 
-# Télécharger Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# Ajouter la clé et le dépôt de Google Chrome
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
 
-# Installer Chrome
-apt-get install -y ./google-chrome-stable_current_amd64.deb
+# Installer Google Chrome
+apt-get update
+apt-get install -y google-chrome-stable
 
-# Créer un lien symbolique à l’emplacement attendu par Selenium
+# Créer le dossier et le lien attendu par Selenium
 mkdir -p /opt/google/chrome
 ln -s /usr/bin/google-chrome /opt/google/chrome/google-chrome
-
-# Nettoyage
-rm google-chrome-stable_current_amd64.deb
