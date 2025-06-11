cd1085c720f5b944fd5a240bde91a6d1146fa727
claudetcam <claudetcamille@gmail.com>
2025-06-11 14:16:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def run_script():
 
     # Étape 2 : Configuration Chrome compatible avec Render
     options = Options()
-    options.binary_location = "/opt/render/project/.render/chrome/opt/google/chrome"
+    options.binary_location = "/usr/bin/google-chrome"
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
